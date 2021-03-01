ee4036b14d6a858ad56b7d6caa24918438cc665a
Adam Luna <adluna@my365.bellevue.edu>
2021-02-28 21:45:55 -0600

    Add 8.3

--- ems/app.js
+++ ems/app.js
@@ -13,8 +13,14 @@ var path = require("path");
 var helmet = require('helmet');
 var logger = require("morgan");
 var mongoose=require("mongoose");
+vaf bodyParser = require("body-parser");
+vaf cookieParser = require("cookie-parser");
+vaf csrf = require("csurf");
 var Employee = require("./models/employee");
 var Schema = mongoose.Schema;
+
+// Add CSRF protection
+const csrfProtection = csrf({ cookie: true });
 
 // Connect to MongoDB
 var mongoDB = "mongodb+srv://admin:thisisapassword@buwebdev-cluster-1.j3npe.mongodb.net/test";
@@ -44,6 +50,21 @@ app.use(helmet.xssFilter());
 
 app.use(express.static(__dirname + "/public"));
 
+app.use(logger("short"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(csrfProtection);
+
+app.use(function (request, response, next) {
+  var token = request.csrfToken();
+  response.cookie("XSRF-TOKEN", token);
+  response.locals.csrfToken = token;
+  next();
+});
+
 app.get("/", function (request, response) {
   response.render("index", {
     message: "Employee Management System",
@@ -66,6 +87,14 @@ app.get("/list", function (request, response) {
   response.render("list", {
     message: "View Employees",
   });
+});
+
+app.post("/process", function(request, response) {
+
+  console.log(request.body.txtName);
+
+  response.redirect("/");
+
 });
 
 http.createServer(app).listen(8080, function () {
